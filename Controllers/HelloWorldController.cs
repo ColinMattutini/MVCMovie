12705519066ad9f7967ab62df3b18568c1a4e28c
Colin <cmmatt14@gmail.com>
2023-03-18 16:26:40 -0400

    controller class added for basic hello world

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller{
+    public string Index(){
+        return "This is the default action...";
+    }
+
+    public string Welcome(string name, int numTimes = 1){
+        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+    }
+}
